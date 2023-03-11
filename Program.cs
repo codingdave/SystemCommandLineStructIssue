c203dfb06beef64f9d4431deb6bc3b2ae75ee2e5
David-Tillmann Schaefer <codingdave@gmail.com>
2023-03-11 22:44:42 +0100

    add issue

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.CommandLine;
+
+internal struct FooStruct {}
+internal class FooClass {}
+
+internal class Program
+{
+    private static Option<FooStruct?> GetFooStructOption() =>
+        new Option<FooStruct?>("", () => new FooStruct());
+    private static Option<FooClass?> GetFooClassOption() =>
+        new Option<FooClass?>("", () => new FooClass());
+
+    private static void Main(string[] args)
+    {
+        var fooStructOption = GetFooStructOption();
+        var fooStructCommand = new Command("fooCommand", "") { fooStructOption};
+        fooStructCommand.SetHandler(FooStructHandler, fooStructOption);
+
+        var fooClassOption = GetFooClassOption();
+        var fooClassCommand = new Command("fooCommand", "") { fooClassOption};
+        fooClassCommand.SetHandler(FooClassHandler, fooClassOption);
+    }
+
+    private static void FooStructHandler(FooStruct fooStruct) {}
+    private static void FooClassHandler(FooClass fooClass) {}
+}
